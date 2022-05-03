b5f3252a9832e3ec4e104d0b728cbfa36d5924b4
Kyle Harline <kylehar9@gmail.com>
2022-05-03 15:32:04 -0600

    annotations endpoint now working. Promised-based

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const methods = require('./lib/methods.js')
 
-let notes = methods.getNotes()
+// let notes = methods.getNotes()
+methods.getNotes()
+// methods.helloWorld()
 
-console.log(notes)
+// console.log(notes)
