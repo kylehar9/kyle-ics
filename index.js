d1839d9403ce96caef7767fdb62751d95d316f77
Kyle Harline <kylehar9@gmail.com>
2022-11-25 19:12:35 -0700

    almost ready for launch.

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ async function main() {
   const tagParams = {
     numberToReturn: 50, setId: 'all', tags: 'BoM: love', type: 'highlight,journal,reference'
   }
+  const setId = '082d144f-e990-46c5-a59a-95b8877f129d'
   const response = await methods.getNotes(tagParams)
   const notes = response.data
 
-  console.log(notes)
-
+  // await methods.moveNotes(notes)
+  await methods.moveNotes(notes, setId)
 }
 
 main()
