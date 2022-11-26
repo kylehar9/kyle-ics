2768417406e2e713fddc1d841cbcaebc047b0dda
Kyle Harline <kylehar9@gmail.com>
2022-11-25 20:26:24 -0700

    response handled in Promise for setting each note. Just need the timeout to work as expected for 🚀

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ const methods = require('./lib/methods.js')
 
 async function main() {
   const tagParams = {
-    numberToReturn: 50, setId: 'all', tags: 'BoM: love', type: 'highlight,journal,reference'
+    numberToReturn: 5, setId: 'all', tags: 'BoM: love', type: 'highlight,journal,reference'
   }
   const setId = '082d144f-e990-46c5-a59a-95b8877f129d'
   const response = await methods.getNotes(tagParams)
   const notes = response.data
 
-  // await methods.moveNotes(notes)
   await methods.moveNotes(notes, setId)
 }
 
