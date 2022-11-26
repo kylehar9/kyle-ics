c4c52ac0ed6a600e767765f0556dd41dfd48b635
Kyle Harline <kylehar9@gmail.com>
2022-11-25 22:46:38 -0700

    ready for liftoff.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const methods = require('./lib/methods.js')
 
 async function main() {
   const tagParams = {
-    numberToReturn: 5, setId: 'all', tags: 'BoM: love', type: 'highlight,journal,reference'
+    numberToReturn: 1000, setId: 'all', tags: 'BoM: love', type: 'highlight,journal,reference'
   }
   const setId = '082d144f-e990-46c5-a59a-95b8877f129d'
   const response = await methods.getNotes(tagParams)
